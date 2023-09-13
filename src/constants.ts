269a776f40aa8ff6f219edde11893dedb9be45e3
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-09-13 16:02:57 +0100

    feat: update payment percentages

--- src/constants.ts
+++ src/constants.ts
@@ -94,10 +94,10 @@ export const MARKETPLACE_FEE = '0.1';
 export const SCRIPT_CREATION_FEE = '0.1';
 export const OPERATOR_REGISTRATION_AR_FEE = '0.05';
 
-export const OPERATOR_PERCENTAGE_FEE = 0.8;
-export const MARKETPLACE_PERCENTAGE_FEE = 0.15;
-export const CURATOR_PERCENTAGE_FEE = 0.025;
-export const CREATOR_PERCENTAGE_FEE = 0.025;
+export const OPERATOR_PERCENTAGE_FEE = 0.7;
+export const MARKETPLACE_PERCENTAGE_FEE = 0.1;
+export const CURATOR_PERCENTAGE_FEE = 0.05;
+export const CREATOR_PERCENTAGE_FEE = 0.15;
 
 export const U_CONTRACT_ID = 'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw';
 export const U_DIVIDER = 1e6;
